acb009dd5cc497cb9ab6068537c2accd44a6f58e
Raphael Bost <raphael_bost@alumni.brown.edu>
2019-04-13 05:53:08 +0200

    Avoid buffer overflows by using scanf_s instead of scanf

--- C.HW.IO/main.c
+++ C.HW.IO/main.c
@@ -1,58 +1,54 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main()
-{
-    /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+int main() {
+  /*
+   * To change this license header, choose License Headers in Project
+   * Properties. To change this template file, choose Tools | Templates and open
+   * the template in the editor.
+   */
 
-/*
- * File:   PrintCompanyInformation.c
- * Author: Velkata
- *
- * Created on 21 Октомври 2015, 14:15
- */
+  /*
+   * File:   PrintCompanyInformation.c
+   * Author: Velkata
+   *
+   * Created on 21 пїЅпїЅпїЅпїЅпїЅпїЅпїЅпїЅ 2015, 14:15
+   */
 
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- *
- */
-int main(int argc, char** argv) {
-    char *companyName,*CompanyAddress,*CompanyNumber,
-            *CFaxNumber,*CwebSite;
+  /*
+   *
+   */
+  int main(int argc, char **argv) {
+    char companyName[1024], CompanyAddress[1024], CompanyNumber[1024],
+        CFaxNumber[1024], CwebSite[1024];
 
-    char *MFname,*MLname,*Mage,*Mphone;
+    char MFname[1024], MLname[1024], Mage[1024], Mphone[1024];
     printf("\nCompany name: ");
-    scanf("%s",companyName);
+    scanf_s("%s", companyName, sizeof(companyName));
     printf("\nCompany address: ");
-    scanf("%s",CompanyAddress);
+    scanf_s("%s", CompanyAddress, sizeof(CompanyAddress));
     printf("\nPhone number: ");
-    scanf("%s",CompanyNumber);
+    scanf_s("%s", CompanyNumber, sizeof(CompanyNumber));
     printf("\nFax number: ");
-    scanf("%s",CFaxNumber);
+    scanf_s("%s", CFaxNumber, sizeof(CFaxNumber));
     printf("\nWeb site: ");
-    scanf("%s",CwebSite);
+    scanf_s("%s", CwebSite, sizeof(CwebSite));
     printf("\nManager first name: ");
-    scanf("%s",MFname);
+    scanf_s("%s", MFname, sizeof(MFname));
     printf("\nManager last name: ");
-    scanf("%s",MLname);
+    scanf_s("%s", MLname, sizeof(MLname));
     printf("\nManager age: ");
-    scanf("%s",Mage);
+    scanf_s("%s", Mage, sizeof(Mage));
     printf("\nManager phone: ");
-    scanf("%s",Mphone);
+    scanf_s("%s", Mphone, sizeof(Mphone));
 
+    printf("%s\nAddress: %s\nTel: %s\nFax: %s\n", companyName, CompanyAddress,
+           CompanyNumber, CFaxNumber);
 
-    printf("%s\nAddress: %s\nTel: %s\nFax: %s\n",
-            companyName,CompanyAddress,CompanyNumber,CFaxNumber);
-
-    printf("Manager: %s %s (age %s,tel: %s)",
-            MFname,MLname,Mage,Mphone);
+    printf("Manager: %s %s (age %s,tel: %s)", MFname, MLname, Mage, Mphone);
 
     return (EXIT_SUCCESS);
-
-}
+  }
